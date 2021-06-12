7be787868cb44e29b84b1bd3218124c34e960587
Blusky <git@blusky.fr>
2021-06-12 09:57:41 +0200

    Change for traefik format

--- allowed.py
+++ allowed.py
@@ -1,71 +1,115 @@
+import asyncio
 import os
 import time
-import sqlite3
+from asyncio import sleep
+
+import yaml
+from yaml import SafeLoader
 
 from logger import log
 
 
 class Allowed(object):
-    def __init__(self, dbfile):
-        first_used = not os.path.isfile(dbfile)
-        self.db = sqlite3.connect(dbfile)
-        self.updating = False
-        if first_used:
-            self.create_db()
+    def __init__(self, ymldatafile, ymlipfile):
+        self.ymldatafile = ymldatafile
+        self.ymlipfile = ymlipfile
+        self.allowed_dict = None
+        self.load_yml()
+        self.waiting_co = None
 
-    def create_db(self):
-        self.db.execute("CREATE TABLE allowed_list (expiration int, ip text, description text)")
-        self.db.commit()
+    def load_yml(self):
+        if not os.path.isfile(self.ymldatafile):
+            self.allowed_dict = {}
+            self.push_data()
+        else:
+            self.load_data()
+
+    def load_data(self):
+        with open(self.ymldatafile, "r") as f:
+            self.allowed_dict = yaml.load(f, Loader=SafeLoader)
+        self.push_data()
+
+    def push_data(self):
+        if self.allowed_dict is None:
+            return
+        with open(self.ymlipfile, "w") as f:
+            yaml.dump(
+                {
+                    "http": {
+                        "middlewares": {
+                            "ipwhitelist": {
+                                "ipWhiteList": {
+                                    "sourceRange": list(self.allowed_dict.keys())
+                                }
+                            }
+                        }
+                    }
+                },
+                f,
+            )
+        with open(self.ymldatafile, "w") as f:
+            yaml.dump(self.allowed_dict, f)
 
     def get_allowed(self):
-        cur = self.db.cursor()
-        cur.execute("SELECT expiration, ip, description, ROWID FROM allowed_list")
         now = int(time.time())
-        allowed = []
-        to_remove = []
-        for r in cur.fetchall():
-            if r[0] == 0 or r[0] > now:
-                allowed.append({
-                    "ip": r[1],
-                    "expiration": r[0],
-                    "desc": r[2],
-                    "id": r[3]
-                })
-            else:
-                to_remove.append({
-                    "ip": r[1],
-                    "expiration": r[0],
-                    "desc": r[2],
-                    "id": r[3]
-                })
-        if len(to_remove) > 0 and not self.updating:
-            for line in to_remove:
-                log("Removing {}".format(line['ip']))
-                self.db.execute(
-                    "DELETE FROM allowed_list WHERE ip=? AND expiration=?",
-                    (line['ip'], line['expiration'])
-                )
-            self.db.commit()
-            self.update_conf(allowed)
-        return allowed
+        refreshed_allowed = {
+            ip: data
+            for ip, data in self.allowed_dict.items()
+            if data["expiration"] == 0 or data["expiration"] > now
+        }
+        if len(refreshed_allowed) != len(self.allowed_dict):
+            self.allowed_dict = refreshed_allowed
+            self.push_data()
+        return self.allowed_dict
 
     def add_allowed(self, ip, desc, expiration):
-        log("Adding {}".format(ip))
-        self.db.execute("INSERT INTO allowed_list (expiration, ip, description) VALUES (?, ?, ?)", (expiration, ip, desc))
-        self.db.commit()
-        self.update_conf()
+        log(f"Adding {ip}")
+        now = int(time.time())
+        if (
+            expiration > 0
+            and expiration - now < self.recommended_waiting_time()
+            and self.waiting_co
+        ):
+            self.waiting_co.cancel()
+        self.allowed_dict[ip] = {
+            "desc": desc,
+            "expiration": expiration,
+        }
+        self.push_data()
 
-    def del_allowed(self, allowed_id):
-        log("Deleting id {}".format(allowed_id))
-        self.db.execute("DELETE FROM allowed_list WHERE ROWID = ?", (allowed_id,))
-        self.db.commit()
-        self.update_conf()
+    def del_allowed(self, ip):
+        log(f"Deleting ip {ip}")
+        if ip in self.allowed_dict:
+            del self.allowed_dict[ip]
+            self.push_data()
 
-    def update_conf(self, allowed=None):
-        self.updating = True
-        if allowed is None:
-            allowed = self.get_allowed()
-        with open("data/allow.conf", "w") as f:
-            for line in allowed:
-                f.write("allow {}; # {} - {} - {}\n".format(line['ip'], line['id'], line['expiration'], line['desc']))
-        self.updating = False
+    def recommended_waiting_time(self):
+        self.get_allowed()
+        now = int(time.time())
+        try:
+            next_expire = min(
+                [
+                    a["expiration"]
+                    for ip, a in self.allowed_dict.items()
+                    if a["expiration"] > 0
+                ]
+            )
+            recommended_waiting_time = next_expire - now
+        except ValueError:
+            recommended_waiting_time = 3600
+        return min(recommended_waiting_time, 3600)
+
+    async def wait_for_expiration(self):
+        if self.waiting_co:
+            raise Exception("Should not happen")
+        recommended_waiting_time = self.recommended_waiting_time()
+        log(f"Waiting for {recommended_waiting_time}")
+        self.waiting_co = asyncio.create_task(sleep(recommended_waiting_time))
+        try:
+            await self.waiting_co
+            self.waiting_co = None
+            return False
+        except asyncio.CancelledError:
+            log(f"Allowed has been updated, refreshing waiting time")
+            self.waiting_co = None
+            return True
--- alohomora.py
+++ alohomora.py
@@ -1,108 +1,121 @@
 import re
 import time
+from asyncio import sleep
+from datetime import timedelta
 import aiohttp_jinja2
 import jinja2
 from aiohttp import web
 import asyncio
 import os
 from aiohttp_session import get_session, setup, SimpleCookieStorage
+from humanize import naturaldelta
+
 from allowed import Allowed
 
-allowed = Allowed("data/db.sqlite")
-token = os.environ["ALOHOMORA_TOKEN"] if "ALOHOMORA_TOKEN" in os.environ else "XXX"
+allowed = Allowed(
+    "data/data.yml",
+    "data/ip-list.yml",
+)
+token = os.environ.get("ALOHOMORA_TOKEN", "XXX")
 
 
-async def download_conf(request):
-    await check_auth(request)
-    response = aiohttp_jinja2.render_template('data/allow.conf', request, {})
-    response.headers['Content-Type'] = 'text/plain'
-    if 'dl' in request.rel_url.query:
-        response.headers['Content-Type'] = 'application/force-download'
-    return response
-
-
-@aiohttp_jinja2.template('templates/login_form.jinja2')
+@aiohttp_jinja2.template("templates/login_form.jinja2")
 async def login_form(request):
     return {}
 
 
 async def login_check(request):
     data = await request.post()
-    if 'token' in data and data['token'] == token:
+    if "token" in data and data["token"] == token:
         session = await get_session(request)
-        session['token'] = data['token']
-        return web.HTTPFound('/')
+        session["token"] = data["token"]
+        return web.HTTPFound("/")
     else:
-        return web.HTTPFound('/login-form')
+        return web.HTTPFound("/login-form")
+
 
 async def check_auth(request):
     session = await get_session(request)
-    if 'token' not in session or session['token'] != token:
-        raise web.HTTPFound('/login-form')
+    if "token" not in session or session["token"] != token:
+        raise web.HTTPFound("/login-form")
 
 
-@aiohttp_jinja2.template('templates/get_allowed.jinja2')
+@aiohttp_jinja2.template("templates/get_allowed.jinja2")
 async def get_allowed(request):
     await check_auth(request)
     allowed_list = allowed.get_allowed()
     if "X-Forwarded-For" in request.headers:
         ip = request.headers["X-Forwarded-For"]
     else:
-        ip = request.transport.get_extra_info('peername')[0]
+        ip = request.transport.get_extra_info("peername")[0]
+    now = int(time.time())
     data = {
-        "allowed_list": allowed_list,
-        "cur_time": int(time.time()),
-        "ip": ip
+        "allowed_list": [
+            {
+                "ip": ip,
+                "human_expiration": naturaldelta(
+                    timedelta(seconds=allowed_elem["expiration"] - now)
+                ),
+                **allowed_elem,
+            }
+            for ip, allowed_elem in allowed_list.items()
+        ],
+        "ip": ip,
     }
     return data
+
 
 async def add_allowed(request):
     await check_auth(request)
     data = await request.post()
-    ip = data['ip']
-    description = data['desc']
-    expiration = 0 if data['expiration'] == "0" else int(data['expiration']) + int(time.time())
+    ip = data["ip"]
+    description = data["desc"]
+    expiration = (
+        0 if data["expiration"] == "0" else int(data["expiration"]) + int(time.time())
+    )
     if not re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip):
-        return web.HTTPFound('/')
+        return web.HTTPFound("/")
     if not re.match(r"^[a-zA-Z0-9.,\- ]*$", description):
-        return web.HTTPFound('/')
+        return web.HTTPFound("/")
     allowed.add_allowed(ip, description, expiration)
-    return web.HTTPFound('/')
+    return web.HTTPFound("/")
+
 
 async def del_allowed(request):
     await check_auth(request)
-    allowed_id = int(request.match_info['allowed_id'])
+    allowed_id = request.match_info["allowed_id"]
     allowed.del_allowed(allowed_id)
-    return web.HTTPFound('/')
+    return web.HTTPFound("/")
 
-async def update_allowed(app):
+
+async def update_allowed():
     try:
+        await sleep(5)
         while True:
+            while await allowed.wait_for_expiration():
+                pass
             allowed.get_allowed()
-            await asyncio.sleep(3600)
     except asyncio.CancelledError:
         pass
-    finally:
-        pass
+
 
 async def start_background_tasks(app):
-    app['allowed_updated'] = app.loop.create_task(update_allowed(app))
+    app["allowed_updated"] = asyncio.create_task(update_allowed())
 
 
 async def cleanup_background_tasks(app):
-    app['allowed_updated'].cancel()
-    await app['allowed_updated']
+    app["allowed_updated"].cancel()
+    await app["allowed_updated"]
 
 
 app = web.Application()
-aiohttp_jinja2.setup(app,loader=jinja2.FileSystemLoader('./'))
-app.router.add_get('/', get_allowed)
-app.router.add_post('/add', add_allowed)
-app.router.add_get('/login-form', login_form)
-app.router.add_get('/allow.conf', download_conf)
-app.router.add_post('/login-check', login_check)
-app.router.add_get('/del/{allowed_id}', del_allowed)
-app.router.add_static('/static', "static")
+aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("./"))
+app.router.add_get("/", get_allowed)
+app.router.add_post("/add", add_allowed)
+app.router.add_get("/login-form", login_form)
+app.router.add_post("/login-check", login_check)
+app.router.add_get("/del/{allowed_id}", del_allowed)
+app.router.add_static("/static", "static")
 setup(app, SimpleCookieStorage())
 app.on_startup.append(start_background_tasks)
 app.on_cleanup.append(cleanup_background_tasks)
@@ -111,5 +124,6 @@ app.on_cleanup.append(cleanup_background_tasks)
 def main():
     web.run_app(app)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
