536cc928d61b5d3263a831bc2376c6cddbeef5fd
Blusky <git@blusky.fr>
2021-10-20 19:51:30 +0200

    Add /auto-add and basic auth

--- alohomora.py
+++ alohomora.py
@@ -1,3 +1,4 @@
+import base64
 import re
 import time
 from asyncio import sleep
@@ -9,14 +10,17 @@ import asyncio
 import os
 from aiohttp_session import get_session, setup, SimpleCookieStorage
 from humanize import naturaldelta
+import uuid
 
 from allowed import Allowed
+from logger import log
 
 allowed = Allowed(
     "data/data.yml",
     "data/ip-list.yml",
 )
-token = os.environ.get("ALOHOMORA_TOKEN", "XXX")
+token = os.environ.get("ALOHOMORA_TOKEN", str(uuid.uuid4()))
+log(f"Starting server with token: {token}")
 
 
 @aiohttp_jinja2.template("templates/login_form.jinja2")
@@ -36,8 +40,14 @@ async def login_check(request):
 
 async def check_auth(request):
     session = await get_session(request)
-    if "token" not in session or session["token"] != token:
-        raise web.HTTPFound("/login-form")
+    if "token" in session and session["token"] == token:
+        return True
+    if auth := request.headers.get("Authorization", None):
+        auth_decoded = base64.b64decode(auth.split(" ")[-1].encode()).decode()
+        auth_token = auth_decoded.split(":")[-1]
+        if auth_token == token:
+            return True
+    raise web.HTTPFound("/login-form")
 
 
 @aiohttp_jinja2.template("templates/get_allowed.jinja2")
@@ -81,6 +91,22 @@ async def add_allowed(request):
     return web.HTTPFound("/")
 
 
+async def auto_add_allowed(request):
+    await check_auth(request)
+    allowed_list = allowed.get_allowed()
+    if "X-Forwarded-For" in request.headers:
+        ip = request.headers["X-Forwarded-For"]
+    else:
+        ip = request.transport.get_extra_info("peername")[0]
+    expiration = int(time.time()) + 3600
+    if ip in allowed_list:
+        allowed_expiration = allowed_list[ip]["expiration"]
+        if allowed_expiration > 0 and allowed_expiration > expiration:
+            return web.HTTPOk()
+    allowed.add_allowed(ip, "Auto added element", expiration)
+    return web.HTTPOk()
+
+
 async def del_allowed(request):
     await check_auth(request)
     allowed_id = request.match_info["allowed_id"]
@@ -112,6 +138,7 @@ app = web.Application()
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("./"))
 app.router.add_get("/", get_allowed)
 app.router.add_post("/add", add_allowed)
+app.router.add_post("/auto-add", auto_add_allowed)
 app.router.add_get("/login-form", login_form)
 app.router.add_post("/login-check", login_check)
 app.router.add_get("/del/{allowed_id}", del_allowed)
